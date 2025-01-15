21702db0f5534e718a8ef7137c358556cc4b585b
Taufik Adi Nugraha <mbairm3512@mbs-MacBook-Air.local>
2025-01-16 02:44:24 +0700

    add intro in Home

--- Home.py
+++ Home.py
@@ -1,4 +1,12 @@
 st.image("images/Logo-Badr-Interactive.png", width=400, use_column_width=False)
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns([1,1,1])
 with col2:
-    st.image("images/Logo-Badr-Interactive.png", width=400) 
+    st.image("images/Logo-Badr-Interactive.png", width=300) 
+
+st.write("""
+    We are excited to introduce our new application featuring both a chatbot and an automatic report generator 
+    powered by Generative AI (GenAI). By harnessing the capabilities of GenAI, we aim to elevate the user 
+    experience and provide a more engaging, interactive environment that goes beyond the traditional dashboard. 
+    With deeper, personalized insights and a dynamic flow of information, users can explore data in fresh, 
+    intuitive ways while saving time and effort through automated reporting.
+""") 
--- streamlit/Home.py
+++ streamlit/Home.py
@@ -9,4 +9,10 @@ st.set_page_config(
 
 col1, col2, col3 = st.columns([1,1,1])
 with col2:
-    st.image("images/Logo-Badr-Interactive.png", width=400) 
+    st.image("images/Logo-Badr-Interactive.png", width=300) 
+
+st.write("""
+We are excited to introduce our new application featuring both a chatbot and an automatic report generator powered by Generative AI (GenAI). 
+By harnessing the capabilities of GenAI, we aim to elevate the user experience and provide a more engaging, interactive environment that goes beyond the traditional dashboard. 
+With deeper, personalized insights and a dynamic flow of information, users can explore data in fresh and intuitive ways while saving time and effort.
+""")
