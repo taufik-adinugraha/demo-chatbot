c2e4b74d1f5209bf2d9aa4df7004d998a022ffd6
Taufik Adi Nugraha <mbairm3512@mbs-MacBook-Air.local>
2025-01-16 06:27:14 +0700

    add intro in Home

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -15,4 +15,8 @@ st.write("""
 We are excited to introduce our new application featuring both a chatbot and an automatic report generator powered by Generative AI (GenAI). 
 By harnessing the capabilities of GenAI, we aim to elevate the user experience and provide a more engaging, interactive environment that goes beyond the traditional dashboard. 
 With deeper, personalized insights and a dynamic flow of information, users can explore data in fresh and intuitive ways while saving time and effort.
+
+This demo showcases a practical application for PLN (Perusahaan Listrik Negara) using simulated electricity consumption data. 
+The database contains hourly usage metrics across different regions and building types, as well as usage counts, total consumption, and minimum/maximum values. 
+This dataset enables us to generate meaningful insights about energy consumption patterns and help identify opportunities for optimization and efficiency improvements.
 """)
